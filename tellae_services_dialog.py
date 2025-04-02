89517c372f129da9044063db1a027cc25bf0768f
leo-desbureaux-tellae <ldesbureaux@tellae.fr>
2025-04-02 15:37:07 +0200

    add starling import

--- tellae_services_dialog.py
+++ tellae_services_dialog.py
@@ -27,6 +27,8 @@ import os
 from qgis.PyQt import uic
 from qgis.PyQt import QtWidgets
 
+from starling_sim.version import __version__
+
 # This loads your .ui file so that PyQt can populate your plugin with the elements from Qt Designer
 FORM_CLASS, _ = uic.loadUiType(os.path.join(
     os.path.dirname(__file__), 'tellae_services_dialog_base.ui'))
