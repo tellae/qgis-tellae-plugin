9363f2ee6a5c5f03b90f6a91aed0fffa9f743227
leo-desbureaux-tellae <ldesbureaux@tellae.fr>
2025-07-17 11:41:42 +0200

    fix merge

--- models/layers.py
+++ models/layers.py
@@ -322,6 +322,7 @@ class QgsKiteLayer:
         elif self.sourceType == "shark":
             return SharkSource(self)
         elif self.sourceType == "vector":
+            if TELLAE_STORE.get_current_scale() > 400000:
                 raise ValueError("You must zoom more to request vector tiles layers")
             return VectorTileGeojsonSource(self)
         else:
