d6141ee0fe2a44d7aa8839dd3255a11571f1cb3b
leo-desbureaux-tellae <ldesbureaux@tellae.fr>
2025-08-08 17:35:44 +0200

    ci: auto publish plugin on release (#26)
    uses the qgis-plugin-ci python package

---
+++ .github/publish_plugin.py
@@ -0,0 +1,4 @@
+from qgispluginci.release import upload_plugin_to_osgeo
+from os import environ
+
+upload_plugin_to_osgeo(environ["OSGEO_USERNAME"], environ["OSGEO_PASSWORD"], environ["ARCHIVE_FILENAME"])
