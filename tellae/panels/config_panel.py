aefa6a64e9baae8d56c3d4f4617f681b9a108fdb
leo-desbureaux-tellae <ldesbureaux@tellae.fr>
2025-12-09 16:21:55 +0100

    add project reload button

--- tellae/panels/config_panel.py
+++ tellae/panels/config_panel.py
@@ -17,6 +17,9 @@ class ConfigPanel(BasePanel):
         # add listener on project selection
         self.dlg.projectSelector.currentIndexChanged.connect(self.select_project_by_name)
 
+        # add listener on project reload button
+        self.dlg.reloadProjectBtn.clicked.connect(self.reload_project)
+
     def set_auth_button_text(self, user):
         if user is None:
             text = "Login"
@@ -37,11 +40,19 @@ class ConfigPanel(BasePanel):
     def select_project_by_name(self, index):
         if self.ongoing_project_selector_fill:
             return
-        log(index)
+
         if index == -1:
             raise ValueError(f"Could not find the project with name")
 
+        self.dlg.start_progress("Récupération des données du projet")
         select_project(self.store.user["_ownedProjects"][index]["uuid"])
+        self.dlg.end_progress()
+
+    def reload_project(self):
+        if self.store.current_project is not None:
+            self.dlg.start_progress("Récupération des données du projet")
+            select_project(self.store.current_project["uuid"])
+            self.dlg.end_progress()
 
     def update_selected_project(self):
         self.dlg.projectDescription.setText(self.store.current_project.get("description", ""))
