2cc1e6624bfb919fd784e372e14069b923c7d3ac
leo-desbureaux-tellae <ldesbureaux@tellae.fr>
2026-06-10 17:03:56 +0200

    use token for publishing

--- .github/publish_plugin.py
+++ .github/publish_plugin.py
@@ -1,6 +1,8 @@
-from qgispluginci.release import upload_plugin_to_osgeo
+from qgispluginci.release import upload_plugin_to_osgeo_with_token
 from os import environ
 
-upload_plugin_to_osgeo(
-    environ["OSGEO_USERNAME"], environ["OSGEO_PASSWORD"], environ["ARCHIVE_FILENAME"]
+upload_plugin_to_osgeo_with_token(
+    environ["ARCHIVE_FILENAME"],
+    "tellae",
+    environ["PUBLISH_TOKEN"],
 )
